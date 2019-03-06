05487cf5abe3138b0bcd93ed217bd87666005cf2
alexmorten <alexandrius.martinus@gmail.com>
2019-03-06 12:24:33 +0100

    add comments to big_bang_config

--- big_bang_config.go
+++ big_bang_config.go
@@ -7,6 +7,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+//BigBangConfig is the structure of the provided  big_bang_config.yaml
 type BigBangConfig struct {
 	SpawnDimension `yaml:"spawn_dimension"`
 	EnergyLevel    uint64 `yaml:"energy_level"`
@@ -14,22 +15,27 @@ type BigBangConfig struct {
 	DnaLengthRange `yaml:"dna_length_range"`
 }
 
+//SpawnDimension ...
 type SpawnDimension struct {
 	Start Vector `yaml:"start"`
 	End   Vector `yaml:"end"`
 }
 
+// DnaLengthRange ...
 type DnaLengthRange struct {
 	Min uint64 `yaml:"min"`
 	Max uint64 `yaml:"max"`
 }
 
+//Vector ...
 type Vector struct {
 	X float32 `yaml:"x"`
 	Y float32 `yaml:"y"`
 	Z float32 `yaml:"z"`
 }
 
+//BigBangConfigFromPath loads the yaml-file the path is pointing to and returns a BigBangConfig with values on success.
+//returns an error if it couldn't load the file or unmarshal the content
 func BigBangConfigFromPath(path string) (*BigBangConfig, error) {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -43,6 +49,7 @@ func BigBangConfigFromPath(path string) (*BigBangConfig, error) {
 	return config, nil
 }
 
+//ToProto from the yaml friendly vector type
 func (v *Vector) ToProto() *proto.Vector {
 	return &proto.Vector{
 		X: v.X,
@@ -51,6 +58,7 @@ func (v *Vector) ToProto() *proto.Vector {
 	}
 }
 
+//ToProto from the yaml friendly SpawnDimension type
 func (d *SpawnDimension) ToProto() *proto.SpawnDimension {
 	return &proto.SpawnDimension{
 		Start: d.Start.ToProto(),
@@ -58,6 +66,7 @@ func (d *SpawnDimension) ToProto() *proto.SpawnDimension {
 	}
 }
 
+//ToProto from the yaml friendly DnaLengthRange type
 func (r *DnaLengthRange) ToProto() *proto.DnaLengthRange {
 	return &proto.DnaLengthRange{
 		Min: r.Min,
@@ -65,6 +74,7 @@ func (r *DnaLengthRange) ToProto() *proto.DnaLengthRange {
 	}
 }
 
+//ToProto from the yaml friendly BigBangConfig type
 func (c *BigBangConfig) ToProto() *proto.BigBangRequest {
 	return &proto.BigBangRequest{
 		SpawnDimension: c.SpawnDimension.ToProto(),
